ed1682ee80353894fcb68dbafab10980337280ad
Hanme Hwang <mustardislove@gmail.com>
2019-10-28 18:04:07 +0900

    in the middle of review create

--- booksbridge/src/containers/CreateReview.js
+++ booksbridge/src/containers/CreateReview.js
@@ -9,6 +9,24 @@ import OCRModal from '../components/OCRModal';
 import AddBookModal from '../components/AddBookModal';
 import Checkbox from '../components/Checkbox';
 import Button from '../components/Button';
+import * as actionCreators from '../store/actions/actionCreators';
+import BookResultSummary from '../components/BookResultSummary/BookResultSummary';
+
+const mapStateToProps = state => {
+  return {
+    selectedBook: state.book.selectedBook,
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchBooks: (keyword, page) => dispatch(actionCreators.getSearchedBooks(keyword, page)),
+    onGetSpecificBook: isbn => dispatch(actionCreators.getSpecificBook(isbn)),
+    onPostShortReview: review => dispatch(actionCreators.postShortReview(review)),
+    onPostLongReivew: review => dispatch(actionCreators.postLongReview(review)),
+    onPostPhrase: review => dispatch(actionCreators.postPhrase(review)),
+  }
+}
 
 class CreateReview extends Component {
 
@@ -19,7 +37,14 @@ class CreateReview extends Component {
     long_review: false,
     phrase: false,
     add_book: false,
-    book: this.props.selectedBook, // from bookDetail
+    book: {isbn: "9788996991342", // 미움받을용기 isbn 
+           authors:  "문병로",
+           contents: "귀납적 사고를 통한 문제 해결 기법 훈련 알고리즘에 대한 지식을 기반으로 제대로 프로그래밍을 하는 이들뿐만 아니라, 알고리즘 속에 깃들어 있는 여러 가지 생각하는 방법, 자료구조, 테크닉을 통해 체계적으로 생각하는 훈련을 하고자 하는 모든 이들을 대상으로 합니다. 알고리즘의 설계와 분석을 활용하여 체계적으로 사고할 수 있는 빌딩 블록을 구축하여 컴퓨터 또는 관련 분야의 연구자 또는 개발자로서 갖춰야 할 지적 기반을 쌓을 수 있습니다.",
+           publisher: "한빛아카데미",
+           thumbnail: "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1566973%3Ftimestamp%3D20190130232605",
+           title: "쉽게 배우는 알고리즘(개정판)(IT Cookbook 한빛 교재 시리즈 229)",
+           }, 
+    //book : this.props.selectedBook, // 1. from BookDetail   or   2. add book in this page by search
     ocr: false,
   }
 
@@ -39,7 +64,11 @@ class CreateReview extends Component {
   onClickCreateButton = () => {
     // create review OR show alert message for inappropriate input 
     if (this.state.title != "" && this.state.content != "") {
-      // something like this.props.createReview
+      this.props.onPostShortReview({
+        isbn: this.state.book.isbn,
+        title: this.state.title,
+        content: this.state.content
+      })
     } else {
       window.alert("input is empty");
     }
@@ -57,8 +86,15 @@ class CreateReview extends Component {
       <Button id="quote" content="Quote" onClick={() => this.setState({ ocr: !this.state.ocr })} />;
     const addBook = !this.state.add_book && (this.state.book == null) &&
       <Button id="add-book" content="Add Book" onClick={() => this.setState({ add_book: !this.state.add_book })} />;
-    // const book = this.state.book;
-    //수정 필요 - 한결
+
+    console.log(this.state.book);
+    const book = <BookResultSummary
+                  cover={this.state.book.thumbnail}
+                  title={this.state.book.title}
+                  authors={this.state.book.authors}
+                  publisher={this.state.book.publisher}
+                  isbn={this.state.book.isbn}
+                 />;
 
     return (
       <div className='CreateReview'>
@@ -82,8 +118,10 @@ class CreateReview extends Component {
         <div>
           {addBook}
           <AddBookModal id="add-book-modal" show={this.state.add_book} whenDone={(book_id) => this.addedBook(book_id)} />
-          {/* {book} */}
+          {book}
+          Title:
           <input id="review-title" onChange={(event) => this.setState({ title: event.target.value })} />
+          Content: 
           <textarea id="review-content" onChange={(event) => this.setState({ content: event.target.value })} />
           {quote}
           <OCRModal id="ocr-modal" show={this.state.ocr} whenDone={() => this.setState({ ocr: !this.state.ocr })} />
@@ -91,20 +129,6 @@ class CreateReview extends Component {
         </div>
       </div>
     );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    selectedBook: state.book.selectedBook,
-  };
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    // 1. createReview (/review)
-    // 2. get book search results (/search)
-    // 3. get book (/book)
   }
 }
 
