f2465124bcefa344279f2c275c0eb5a893007db4
PeaceSong <driveshoot@snu.ac.kr>
2019-11-02 21:43:16 +0900

    completed book detail

--- booksbridge/src/components/BookTabsReview.js
+++ booksbridge/src/components/BookTabsReview.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import Article from "./Article";
 
 import "./BookTabsReview.css";
 
@@ -8,49 +9,41 @@ class BookTabsReview extends Component {
   componentDidMount() {}
 
   render() {
-		if(this.props.reviews.length == 0){
-			return(
-				<div>
-					아직 리뷰가 없어요... 하나 작성해보시면 어떨까요?
-				</div>
-			);
-		}
-
-
-		const reviews = this.props.reviews.map((review) => {
-			console.log("[DEBUG] review.title: " + review.title + "review.content: " + review.content);
-			return(
-				<div>
-					<p>-------------------------------------------------------------------------------------</p>
-					<h1>{review.title}</h1>
-					<h3>{review.author}</h3>
-					<h4>{review.date}</h4>
-					<p>{review.content}</p>
-					<p>-------------------------------------------------------------------------------------</p>
-				</div>
-			);
-		});
-
-
-    if (this.props.is_short) {
-      return(
-        <div>{reviews}</div>
-      );
+    if (this.props.reviews.length == 0) {
+      return <div>아직 리뷰가 없어요... 하나 작성해보시면 어떨까요?</div>;
     }
 
-    if (this.props.is_long){
-      return(
-				<div>{reviews}</div>
-      );
-		}
-		
-		if(this.props.is_phrase){
-			return(
-				<div>{reviews}</div>
-			);
-		}
+    const reviews = this.props.reviews.map(review => {
+      console.log("[DEBUG] review.title: " + review.title + "review.content: " + review.content);
 
-	}
+      return (
+        <div>
+          <Article
+            author={review.author}
+            title={review.title}
+            book={review.book}
+            content={review.content}
+            date={review.date}
+            is_long={review.is_long}
+            is_short={review.is_short}
+            is_phrase={review.is_phrase}
+          />
+        </div>
+      );
+    });
+
+    if (this.props.is_short) {
+      return <div>{reviews}</div>;
+    }
+
+    if (this.props.is_long) {
+      return <div>{reviews}</div>;
+    }
+
+    if (this.props.is_phrase) {
+      return <div>{reviews}</div>;
+    }
+  }
 }
 
 const mapStateToProps = state => {
