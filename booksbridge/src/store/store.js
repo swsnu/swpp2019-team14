3d55324c116de8dd62233f0b2279e7ee4649da74
zlzlqlzl <zlzlqlzl1@naver.com>
2019-11-04 21:02:35 +0900

    test code update

---
+++ booksbridge/src/store/store.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';                                           
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+
+import curationReducer from './reducers/curation'
+import articleReducer from './reducers/article'
+import userReducer from './reducers/user'
+import bookReducer from './reducers/book'
+
+export const history = createBrowserHistory();
+const rootReducer = combineReducers({
+  book: bookReducer,
+  user: userReducer,
+  curation: curationReducer,
+  article: articleReducer,
+  router: connectRouter(history),
+});
+export const middlewares = [thunk, routerMiddleware(history)]
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,
+  composeEnhancers(
+    applyMiddleware(...middlewares)));
+
+export default store;
