34c3fcf3ba98a33ab79e376aa61736a04e885413
PeaceSong <driveshoot@snu.ac.kr>
2019-11-14 01:17:37 +0900

    testing duplicacy2

--- booksbridge/src/components/UserPage/UserReviewList.js
+++ booksbridge/src/components/UserPage/UserReviewList.js
@@ -11,8 +11,6 @@ class UserReviewList extends Component {
   render() {
     if (!this.props.user_reviews) return null;
 
-    console.log('[DEBUG]' + this.props.user_reviews);
-
     const reviews = this.props.user_reviews.map(review => {
       return (
         <Article
@@ -31,193 +29,29 @@ class UserReviewList extends Component {
       );
     });
 
-    const shortReviewList = reviews.filter(review => review.props.is_short);
-    const longReviewList = reviews.filter(review => review.props.is_long);
-    const phraseList = reviews.filter(review => review.is_phrase);
-
-    /*
-    const shortReviewList = this.props.user_reviews
-      .filter(review => review.is_short)
-      .map(review => {
-        return (
-          <Article
-            author={review.author}
-            book_isbn={review.book_isbn}
-            book_title={review.book_title}
-            book_thumbnail={review.book_thumbnail}
-            id={review.id}
-            title={review.title}
-            content={review.content}
-            date={review.date}
-            is_long={review.is_long}
-            is_short={review.is_short}
-            is_phrase={review.is_phrase}
-          />
-        );
-      });
-    const longReviewList = this.props.user_reviews
-      .filter(review => review.is_long)
-      .map(review => {
-        return (
-          <Article
-            author={review.author}
-            book_isbn={review.book_isbn}
-            book_title={review.book_title}
-            book_thumbnail={review.book_thumbnail}
-            id={review.id}
-            title={review.title}
-            content={review.content}
-            date={review.date}
-            is_long={review.is_long}
-            is_short={review.is_short}
-            is_phrase={review.is_phrase}
-          />
-        );
-      });
-    const phraseList = this.props.user_reviews
-      .filter(review => review.is_phrase)
-      .map(review => {
-        return (
-          <Article
-            author={review.author}
-            book_isbn={review.book_isbn}
-            book_title={review.book_title}
-            book_thumbnail={review.book_thumbnail}
-            id={review.id}
-            title={review.title}
-            content={review.content}
-            date={review.date}
-            is_long={review.is_long}
-            is_short={review.is_short}
-            is_phrase={review.is_phrase}
-          />
-        );
-      });
-
-    /*
-        this.props.user_reviews.forEach(review => {
-          console.log('[DEBUG] in forEach');
-          const article = (
-            <Article
-              author={review.author}
-              book_isbn={review.book_isbn}
-              book_title={review.book_title}
-              book_thumbnail={review.book_thumbnail}
-              id={review.id}
-              title={review.title}
-              content={review.content}
-              date={review.date}
-              is_long={review.is_long}
-              is_short={review.is_short}
-              is_phrase={review.is_phrase}
-            />
-          );
-    
-          if (review.is_short) {
-            console.log('[DEBUG] review is short');
-            shortReviewList.concat(article);
-            console.log(shortReviewList);
-          } else if (review.is_long) {
-            console.log('[DEBUG] review is long');
-            longReviewList.concat(article);
-            console.log(shortReviewList);
-          } else phraseList.concat(article);
-        });
-    
-        console.log('[DEBUG]' + shortReviewList);
-        console.log('[DEBUG]' + longReviewList);
-        console.log('[DEBUG]' + phraseList);
-    */
-    /*
-    const shortReviewList = this.props.user_reviews.reduce(function(
-      accumulator,
-      currentValue,
-    ) {
-      if (currentValue.is_short) {
-        return accumulator.concat(
-          <Article
-            author={currentValue.author}
-            book_isbn={currentValue.book_isbn}
-            book_title={currentValue.book_title}
-            book_thumbnail={currentValue.book_thumbnail}
-            id={currentValue.id}
-            title={currentValue.title}
-            content={currentValue.content}
-            date={currentValue.date}
-            is_long={currentValue.is_long}
-            is_short={currentValue.is_short}
-            is_phrase={currentValue.is_phrase}
-          />,
-        );
-      } else {
-        return accumulator;
-      }
-    },
-    []);
-
-    const longReviewList = this.props.user_reviews.reduce(function(
-      accumulator,
-      currentValue,
-    ) {
-      if (currentValue.is_long) {
-        return accumulator.concat(
-          <Article
-            author={currentValue.author}
-            book_isbn={currentValue.book_isbn}
-            book_title={currentValue.book_title}
-            book_thumbnail={currentValue.book_thumbnail}
-            id={currentValue.id}
-            title={currentValue.title}
-            content={currentValue.content}
-            date={currentValue.date}
-            is_long={currentValue.is_long}
-            is_short={currentValue.is_short}
-            is_phrase={currentValue.is_phrase}
-          />,
-        );
-      } else {
-        return accumulator;
-      }
-    },
-    []);
-
-    const phraseList = this.props.user_reviews.reduce(function(
-      accumulator,
-      currentValue,
-    ) {
-      if (currentValue.is_phrase) {
-        return accumulator.concat(
-          <Article
-            author={currentValue.author}
-            book_isbn={currentValue.book_isbn}
-            book_title={currentValue.book_title}
-            book_thumbnail={currentValue.book_thumbnail}
-            id={currentValue.id}
-            title={currentValue.title}
-            content={currentValue.content}
-            date={currentValue.date}
-            is_long={currentValue.is_long}
-            is_short={currentValue.is_short}
-            is_phrase={currentValue.is_phrase}
-          />,
-        );
-      } else {
-        return accumulator;
-      }
-    },
-    []);
-  */
+    //const shortReviewList = reviews.filter(review => review.props.is_short);
+    //const longReviewList = reviews.filter(review => review.props.is_long);
+    //const phraseList = reviews.filter(review => review.is_phrase);
 
     const panes = [
       {
         menuItem: 'Short Review',
-        render: () => <Tab.Pane>{shortReviewList}</Tab.Pane>,
+        render: () => (
+          <Tab.Pane>{reviews.filter(review => review.props.is_short)}</Tab.Pane>
+        ),
       },
       {
         menuItem: 'Long Review',
-        render: () => <Tab.Pane>{longReviewList}</Tab.Pane>,
+        render: () => (
+          <Tab.Pane>{reviews.filter(review => review.props.is_long)}</Tab.Pane>
+        ),
       },
-      { menuItem: 'Phrase', render: () => <Tab.Pane>{phraseList}</Tab.Pane> },
+      {
+        menuItem: 'Phrase',
+        render: () => (
+          <Tab.Pane>{reviews.filter(review => review.is_phrase)}</Tab.Pane>
+        ),
+      },
     ];
 
     return (
