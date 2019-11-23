3fab91d336483c91b080d5640e0f4ddd29d04988
zlzlqlzl <zlzlqlzl1@naver.com>
2019-11-24 03:58:14 +0900

    curation main update : choi - 11/24

--- booksbridge/src/components/CurationSummary/CurationSummary.js
+++ booksbridge/src/components/CurationSummary/CurationSummary.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import { withRouter } from 'react-router';
-import { Button, Feed, Icon } from 'semantic-ui-react';
+import { Button, Feed, Icon, Image } from 'semantic-ui-react';
 import Alert from 'react-bootstrap/Alert';
 import Time from '../Time';
 import ProfileSummary from '../ProfileSummary/ProfileSummary';
 import Carousel from 'react-bootstrap/Carousel';
+import './CurationSummary.css';
 
 const CurationSummary = props => {
   const Author = <ProfileSummary user={props.author} />;
 
-  // const Books = props.curations.books.map(book => {
-  //   return (
-  //     <Carousel.Item>
-  //       <img
-  //         className="d-block w-100"
-  //         src={book.book_thumbnail}
-  //         alt="First slide"
-  //       />
-  //       <Carousel.Caption>
-  //         <h3>First slide label</h3>
-  //         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-  //       </Carousel.Caption>
-  //     </Carousel.Item>
-  //   );
-  // });
+  const Books = props.books.map(book_set => {
+    return (
+      <Carousel.Item className="curation-summary-items">
+        <Image.Group className="curation-summary-images">
+          {book_set[0] ? (
+            <Image
+              href="http://google.com"
+              src={book_set[0].thumbnail}
+              alt="First slide"
+              centered
+            />
+          ) : null}
+          {book_set[1] ? (
+            <Image src={book_set[1].thumbnail} alt="First slide" centered />
+          ) : null}
+          {book_set[2] ? (
+            <Image src={book_set[2].thumbnail} alt="First slide" centered />
+          ) : null}
+          {book_set[3] ? (
+            <Image src={book_set[3].thumbnail} alt="First slide" centered />
+          ) : null}
+          {book_set[4] ? (
+            <Image src={book_set[4].thumbnail} alt="First slide" centered />
+          ) : null}
+        </Image.Group>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    );
+  });
 
   return (
     <Alert variant="secondary" className="article">
@@ -42,8 +60,18 @@ const CurationSummary = props => {
                   <h3 className="MainArticleTitle">{props.title}</h3>
                 </a>
                 <div className="MainArticleContent">
-                  <div className="content">
-                    {/* <Carousel>{Books}</Carousel> */}
+                  <div className="curation-summary-content">
+                    <Carousel
+                      className="carousel-items"
+                      prevIcon={
+                        <Icon color="black" name="angle left" size="large" />
+                      }
+                      nextIcon={
+                        <Icon color="black" name="angle right" size="large" />
+                      }
+                    >
+                      {Books}
+                    </Carousel>
                   </div>
                 </div>
               </div>
