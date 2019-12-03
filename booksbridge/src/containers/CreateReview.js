058232f5876f2814b88c0cc5e9b1ec4e80089540
Bae yoo bin <yb_zzang@snu.ac.kr>
2019-12-03 17:02:32 +0900

    Add Popup in creating review

--- booksbridge/src/containers/CreateReview.js
+++ booksbridge/src/containers/CreateReview.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import { Button, Form, TextArea } from 'semantic-ui-react';
+import { Popup, Button, Form, TextArea } from 'semantic-ui-react';
 
 import Header from '../components/Header';
 import OcrModal from '../components/OcrModal/OcrModal';
@@ -64,57 +64,79 @@ class CreateReview extends Component {
         publisher={this.props.selectedBook.publisher}
         isbn={this.props.selectedBook.isbn}
         direct={false}
-        click={() => { }}
+        click={() => {}}
       />
     ) : null;
+
+    const LongReviewCheckbox = (
+      <div className="field">
+        <div className="ui radio checkbox">
+          <input
+            type="radio"
+            id="long-review-radio"
+            type="radio"
+            name="radioGroup"
+            value="long-review"
+            checked={this.state.type === 'long-review'}
+            onChange={this.radioHandler}
+          />
+          <label>Long Review</label>
+        </div>
+      </div>
+    );
+
+    const ShortReviewCheckbox = (
+      <div className="field">
+        <div className="ui radio checkbox">
+          <input
+            type="radio"
+            id="short-review-radio"
+            type="radio"
+            name="radioGroup"
+            value="short-review"
+            checked={this.state.type === 'short-review'}
+            onChange={this.radioHandler}
+          />
+          <label>Short Review</label>
+        </div>
+      </div>
+    );
+
+    const PhraseCheckbox = (
+      <div className="field">
+        <div className="ui radio checkbox">
+          <input
+            type="radio"
+            id="phrase-radio"
+            type="radio"
+            name="radioGroup"
+            value="phrase"
+            checked={this.state.type === 'phrase'}
+            onChange={this.radioHandler}
+          />
+          <label>Phrase</label>
+        </div>
+      </div>
+    );
+
     return (
       <div className="CreateReview">
         <Header />
         <div className="ReviewTypeCheckbox">
           <div className="ui form">
             <div className="inline fields">
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    id="long-review-radio"
-                    type="radio"
-                    name="radioGroup"
-                    value="long-review"
-                    checked={this.state.type === 'long-review'}
-                    onChange={this.radioHandler}
-                  />
-                  <label>Long Review</label>
-                </div>
-              </div>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    id="short-review-radio"
-                    type="radio"
-                    name="radioGroup"
-                    value="short-review"
-                    checked={this.state.type === 'short-review'}
-                    onChange={this.radioHandler}
-                  />
-                  <label>Short Review</label>
-                </div>
-              </div>
-              <div className="field">
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    id="phrase-radio"
-                    type="radio"
-                    name="radioGroup"
-                    value="phrase"
-                    checked={this.state.type === 'phrase'}
-                    onChange={this.radioHandler}
-                  />
-                  <label>Phrase</label>
-                </div>
-              </div>
+              <Popup
+                content="제목이 있고, 문단을 갖춘 긴 리뷰를 작성합니다. 긴 리뷰는 140자를 넘어야 합니다."
+                trigger={LongReviewCheckbox}
+              />
+              <Popup
+                content="140자 미만의 짧은 리뷰를 작성할 수 있습니다."
+                trigger={ShortReviewCheckbox}
+              />
+              <Popup
+                content="인상 깊었던 책의 구절, 문장 혹은 문단을 모두와 공유해보세요."
+                trigger={PhraseCheckbox}
+              />
             </div>
           </div>
         </div>
