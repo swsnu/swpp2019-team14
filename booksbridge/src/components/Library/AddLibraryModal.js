42f610d6ffa81cf95d48ba10c42fee96399ff70c
PeaceSong <driveshoot@snu.ac.kr>
2019-12-06 14:19:25 +0900

    AddLibraryModal fixed

--- booksbridge/src/components/Library/AddLibraryModal.js
+++ booksbridge/src/components/Library/AddLibraryModal.js
@@ -22,6 +22,7 @@ class AddLibraryModal extends Component {
   constructor(props) {
     super(props);
   }
+
   state = {
     open: false,
     title: this.props.title ? this.props.title : '',
@@ -62,12 +63,14 @@ class AddLibraryModal extends Component {
       console.log(book.title);
     });
     console.log(
-      '[DEBUG] selectedBook exists?: ' + this.props.selectedBook != null
-        ? 'yes. the title is: ' + this.props.selectedBook
-        : 'no',
+      '[DEBUG] selectedBook exists?: ' +
+        (this.props.selectedBook != null
+          ? 'yes. the title is: ' + this.props.selectedBook.title
+          : 'no'),
     );
     //if (this.props.selectedBook == null) return;
     /* Debug End */
+
     setTimeout(() => {
       console.log('[DEBUG] timeout');
 
