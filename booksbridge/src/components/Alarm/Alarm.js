001a059b8e1eb7b34b3272908f05f115c432c05f
zlzlqlzl <zlzlqlzl1@naver.com>
2019-12-11 03:41:02 +0900

    mobile css & minor update

--- booksbridge/src/components/Alarm/Alarm.js
+++ booksbridge/src/components/Alarm/Alarm.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './Alarm.css';
 const Alarm = props => {
-  let photo;
-  if (props.alarm.profile_photo.startsWith('http'))
-    photo = props.alarm.profile_photo;
-  else photo = '/media/' + props.alarm.profile_photo;
   return (
     <div className={props.alarm.is_new ? 'new-alarm' : 'old-alarm'}>
       <a
@@ -12,7 +8,14 @@ const Alarm = props => {
         onClick={() => props.onClickAlarm(props.alarm.id)}
       >
         <a href={'/page/' + props.alarm.author_username}>
-          <img className="profilePic" src={props.alarm.profile_photo} />
+          <img
+            className="profilePic"
+            src={
+              props.alarm.profile_photo.startsWith('http')
+                ? props.alarm.profile_photo
+                : '/media/' + props.alarm.profile_photo
+            }
+          />
         </a>
         {props.alarm.content}
       </a>
