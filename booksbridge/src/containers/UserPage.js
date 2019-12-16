a055722c8d5d6fcf332dc9404f5e684b820d7462
Bae yoo bin <yb_zzang@snu.ac.kr>
2019-12-16 12:27:24 +0900

    좋아하는 책 한 줄에 5권

--- booksbridge/src/containers/UserPage.js
+++ booksbridge/src/containers/UserPage.js
@@ -101,9 +101,9 @@ class UserPage extends Component {
         : parseInt(this.props.CurationLength / 5) + 1;
 
     let BookFinal =
-      like_books.length % 6 === 0
-        ? like_books.length / 6
-        : parseInt(like_books.length / 6) + 1;
+      like_books.length % 5 === 0
+        ? like_books.length / 5
+        : parseInt(like_books.length / 5) + 1;
 
     let BookmarkFinal =
       this.props.BookmarkLength % 5 === 0
@@ -208,8 +208,8 @@ class UserPage extends Component {
 
     let slicedBooks = [];
     slicedBooks = like_books.slice(
-      (this.state.activeBookPage - 1) * 6,
-      this.state.activeBookPage * 6,
+      (this.state.activeBookPage - 1) * 5,
+      this.state.activeBookPage * 5,
     );
 
     const likebooks = slicedBooks.map(book => {
